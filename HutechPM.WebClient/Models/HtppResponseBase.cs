2762664a7865759659d3bf1175e0f5a728b6b6b6
trongvuong0108 <tronghandsome1111@gmail.com>
2024-01-19 19:32:37 +0700

    upload

---
+++ HutechPM.WebClient/Models/HtppResponseBase.cs
@@ -0,0 +1,9 @@
+﻿namespace HutechPM.WebClient.Models
+{
+    public class HtppResponseBase<T> 
+    {
+        public int ErrorCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public T? DataObject { get; set; }
+    }
+}
